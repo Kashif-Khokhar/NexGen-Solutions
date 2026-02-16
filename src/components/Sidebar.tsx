64fe632a3ae5e6001349fe924d90b2c2887d4e75
Kashif-Khokhar <kashifkhokhar1199@gmail.com>
2026-02-16 18:47:50 +0500

    feat: add responsive and collapsible sidebar navigation and initial home page structure

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar = ({ onCollapse }: { onCollapse: (collapsed: boolean) => void }) => {
     const [isCollapsed, setIsCollapsed] = useState(false);
